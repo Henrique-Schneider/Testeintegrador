df1c72336116587d9adf36e9ec0d0800af8ac864
Henrique-Schneider <domeneider@gmail.com>
2023-03-24 17:44:02 -0300

    subindo projeto TEste

---
+++ src/main/java/br/com/teste/Testeintegrador/TesteintegradorApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste.Testeintegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteintegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteintegradorApplication.class, args);
+	}
+
+}
